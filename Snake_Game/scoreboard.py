94c982ab89267617e9d12d1d2a2813b7e4a4e18f
Shashank-Singh <shashank9354@gmail.com>
2025-03-07 19:19:31 +0530

    Update scoreboard.py
    added a reset function to reset the score and highscore if the score is more than the highscore

--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -6,24 +6,34 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highScore = 0
         self.color("White")
         self.penup()
         self.goto(0,260)
-        
         self.hideturtle()
         self.updateScore()
 
     def updateScore(self):
-                self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.highScore}", move=False, align=ALIGNMENT, font=FONT)
+                
+    def reset(self):
+        if self.score>self.highScore:
+            self.highScore=self.score
+        self.score = 0
+        self.updateScore()
 
     def increaseScore(self):
         self.score +=1
         self.clear()
         self.updateScore()
     
-    def gameOver(self):
-          self.goto(0,0)
-          self.write(f"GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+
+    
+
+        
+
+    
 
     
     
